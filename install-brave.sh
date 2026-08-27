71d2bc58bf692369c675a5240857520f1514c869
khaldoun <info@khaldoun.xyz>
2026-08-27 07:35:13 +0200

    :sparkles: set brave as default browser

--- install-brave.sh
+++ install-brave.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 yay -S --noconfirm brave-bin
-echo "✨ Brave Browser installed."
+
+# Set Brave as the default browser (run as user, not sudo)
+xdg-settings set default-web-browser brave-browser.desktop
+xdg-mime default brave-browser.desktop x-scheme-handler/http x-scheme-handler/https text/html application/xhtml+xml
+
+echo "✨ Brave Browser installed and set as default."
